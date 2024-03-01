692f1cf632dfa7151113342d4cf7518e23316e3c
donbosenga <bosengad@gmail.com>
2024-03-01 17:41:57 +0100

    First push

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!";
+    srand(time(nullptr));
+    int minVal = 1;
+    int maxVal = 6;
+
+    int firstDice = (rand() % (maxVal - minVal + 1) + minVal);
+    int secondDice = (rand() % (maxVal - minVal + 1) + minVal);
+
+    cout << "The first player played: " << firstDice << endl
+         << "The second player played: " << secondDice << endl;
+
+    if(firstDice > secondDice){
+        cout << "The first player won";
+    }
+    else if(secondDice>firstDice){
+        cout << "The second player won";
+    }
+    else{
+        cout << "Draw between them";
+    }
+
     return 0;
 }

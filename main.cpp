dabd91137619c919f3e4fe6c2ffc9107848a5388
donbosenga <bosengad@gmail.com>
2024-03-06 11:57:06 +0100

    make a better game

--- main.cpp
+++ main.cpp
@@ -2,28 +2,73 @@
 #include <cstdlib>
 #include <ctime>
 
-using namespace std;
+constexpr int minVal = 1;
+constexpr int maxVal = 6;
+
+int rollDice() {
+    return std::rand() % (maxVal - minVal + 1) + minVal;
+}
 
 int main() {
-    srand(time(nullptr));
-    int minVal = 1;
-    int maxVal = 6;
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
 
-    int firstDice = (rand() % (maxVal - minVal + 1) + minVal);
-    int secondDice = (rand() % (maxVal - minVal + 1) + minVal);
+    int rounds = 3;
+    std::string name1, name2 ;
+    std::cout << "Player 1 enter your name: ";
+    std::cin >> name1;
+    std::cout << "Player 2 enter your name: ";
+    std::cin >> name2;
+    std::cout << "Enter the number of rounds: ";
+    std::cin >> rounds;
 
-    cout << "The first player played: " << firstDice << endl
-         << "The second player played: " << secondDice << endl;
 
-    if(firstDice > secondDice){
-        cout << "The first player won";
+    int player1Score = 0, player2Score = 0;
+
+    for (int round = 1; round <= rounds; ++round) {
+        if(rounds > 3){
+            std::cout << "Cannot be greater than 3 rounds" << '\n';
+            break;
+        }
+
+        std::cout << '\n' << "Round " << round << ":\n";
+
+
+        int firstDice = rollDice();
+        int secondDice = rollDice();
+
+        std::cout << name1 <<" rolled: " << firstDice << '\n'
+                  << name2  << " rolled: " << secondDice << '\n';
+
+        if (firstDice > secondDice) {
+            std::cout << name1 << " wins this round!\n";
+            ++player1Score;
+        } else if (secondDice > firstDice) {
+            std::cout << name2 << " wins this round!\n";
+            ++player2Score;
+        } else {
+            std::cout << "It's a draw this round!\n";
+        }
+
+        std::cout << "Current Score: " << name1 << " = "  << player1Score << '\n'
+                  << name2 << " = " << player2Score << "\n\n";
     }
-    else if(secondDice>firstDice){
-        cout << "The second player won";
+
+    if (rounds > 3){
+        std::cout << "Game Over";
     }
     else{
-        cout << "Draw between them";
+        std::cout << "Final Score: " << '\n' << name1 << " = " << player1Score << '\n'
+                  << name2 << " = " << player2Score << '\n';
+
+        if (player1Score > player2Score) {
+            std::cout << name1 << " wins the game!\n";
+        } else if (player2Score > player1Score) {
+            std::cout << name2 << " wins the game!\n";
+        } else {
+            std::cout << "The game is a draw!\n";
+        }
     }
 
     return 0;
 }
+
